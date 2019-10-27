3cc2c3c5bdd6f62f94d37640293a37d88443ef3d
Paaka <Mateuszhartabus@gmail.com>
2019-10-27 21:33:21 +0100

    finished random seed generator

--- Canvas-sketch-tutorial/sketch.js
+++ Canvas-sketch-tutorial/sketch.js
@@ -12,15 +12,16 @@ const sketch = () => {
   console.log(palette);
   const createGrid = () => {
       const points = [];
-      const count = 20;
+      const count = 40;
       for(let x =0; x <count; x++){
         for(let y=0; y <count; y++){
           const u = count <= 1 ? 0.5 : x / (count - 1);
           const v = count <= 1 ? 0.5 : y / (count - 1);
-          const radius = Math.abs(random.noise2D(u,v)) * 0.07;
+          const radius = Math.abs(random.noise2D(u,v)) * 0.22;
           points.push({
             color: random.pick(palette),
             radius,
+            rotation: random.noise2D(u,v),
             position: [u , v]})
         }
        
@@ -28,7 +29,7 @@ const sketch = () => {
       return points;
   };
   
-  random.setSeed(5211);
+ 
   const points = createGrid().filter(()=> random.value() > 0.5);
   const margin = 400;
 
@@ -40,17 +41,26 @@ const sketch = () => {
       const {
         position,
         radius,
-        color
+        color,
+        rotation
       } = data;
 
       const [u ,v] = position;
       const x = lerp(margin, width-margin, u);
       const y = lerp(margin, height-margin, v);
 
-      context.beginPath();
-      context.arc(x,y, radius*width, 0, Math.PI *2, false);
+      // context.beginPath();
+      // context.arc(x,y, radius*width, 0, Math.PI *2, false);
+      // context.fillStyle = color;
+      // context.fill();
+
+      context.save();
       context.fillStyle = color;
-      context.fill();
+      context.font = `${radius * width}px "Helvetica"`
+      context.translate(x,y)
+      context.rotate(rotation);
+      context.fillText('=', 0,0);
+      context.restore();
     });
   };
 };
