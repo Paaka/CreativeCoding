f08112f8bc23d88ba922db5698cef7713c29990e
Paaka <Mateuszhartabus@gmail.com>
2019-10-27 20:41:46 +0100

    added canvas-sketch-util

--- Canvas-sketch-tutorial/sketch.js
+++ Canvas-sketch-tutorial/sketch.js
@@ -1,22 +1,52 @@
 const canvasSketch = require('canvas-sketch');
+const { lerp } = require('canvas-sketch-util/math')
+const random = require('canvas-sketch-util/random')
 
 const settings = {
-  dimensions: 'A4',
+  dimensions: [2048,2048],
 };
 
 const sketch = () => {
+  const createGrid = () => {
+      const points = [];
+      const count = 30;
+      for(let x =0; x <count; x++){
+        for(let y=0; y <count; y++){
+          const u = count <= 1 ? 0.5 : x / (count - 1);
+          const v = count <= 1 ? 0.5 : y / (count - 1);
+          points.push({
+            radius: random.value(),
+            position: [u , v]})
+        }
+        console.log(points)
+      }
+      return points;
+  };
+  
+  random.setSeed(576);
+  const points = createGrid().filter(()=> random.value() > 0.5);
+  const margin = 400;
+
+
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
-    context.fillRect(0,0, width, height)
+    context.fillRect(0,0, width, height);
+    points.forEach(data =>{
+      const {
+        position,
+        radius
+      } = data;
 
-    context.beginPath();
-    context.arc(width /2, height /2, 500,  0, Math.PI * 2, false);
-    context.fillStyle = 'orangered';
-    context.fill();
+      const [u ,v] = position;
+      const x = lerp(margin, width-margin, u);
+      const y = lerp(margin, height-margin, v);
 
-    context.lineWidth = 10;
-    context.strokeStyle = 'blue';
-    context.stroke();
+      context.beginPath();
+      context.arc(x,y, 10, 0, Math.PI *2, false);
+      context.lineWidth = 20;
+      context.strokeStyle = 'royalblue';
+      context.stroke();
+    });
   };
 };
 
