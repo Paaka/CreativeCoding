249aedfe2788d033aac3bd7c39a13883f0aca4b0
Paaka <Mateuszhartabus@gmail.com>
2019-11-05 16:11:15 +0100

    Changed diminesions to A4

--- Canvas-sketch-tutorial/sketch.js
+++ Canvas-sketch-tutorial/sketch.js
@@ -4,7 +4,7 @@ const random = require('canvas-sketch-util/random');
 const palettes = require('nice-color-palettes');
 
 const settings = {
-  dimensions: [2048,2048],
+  dimensions: 'A4',
 };
 
 const sketch = () => {
@@ -12,12 +12,12 @@ const sketch = () => {
   console.log(palette);
   const createGrid = () => {
       const points = [];
-      const count = 40;
+      const count = 50;
       for(let x =0; x <count; x++){
         for(let y=0; y <count; y++){
           const u = count <= 1 ? 0.5 : x / (count - 1);
           const v = count <= 1 ? 0.5 : y / (count - 1);
-          const radius = Math.abs(random.noise2D(u,v)) * 0.22;
+          const radius = Math.abs(random.noise2D(u,v)) * 0.15;
           points.push({
             color: random.pick(palette),
             radius,
@@ -31,11 +31,11 @@ const sketch = () => {
   
  
   const points = createGrid().filter(()=> random.value() > 0.5);
-  const margin = 400;
+  const margin = 300;
 
 
   return ({ context, width, height }) => {
-    context.fillStyle = 'white';
+    context.fillStyle = '#eee';
     context.fillRect(0,0, width, height);
     points.forEach(data =>{
       const {
